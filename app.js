ce586a556b16efceb66777194c721c83fe3bc1fa
Eric Dobyns <ericdobyns@gmail.com>
2017-02-18 10:47:01 -0800

    Fixed app.js

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 'use strict';
-              
-var express = require('express');
-var app = express();
-var router = express.Router();
 
-process.env.PORT = 8080;
+var express = require('express')
+var app = express()
 
-// Initialize Server
-app.listen(process.env.PORT, function() {
-  console.log('Wooooooo Hoooooooooo!!!');
-  console.log('Wooooooo Hoooooooooo!!!');
-  console.log('Listening on port:' + process.env.PORT);
-});
- 
-// Handle Errors
-app.use(function(req, res) {
-  res.status(404);
-  res.sendFile('404.html', { root: require('path').join(__dirname, 'public/views/misc') });
-  console.log('Error: /404 loaded');
-});
+  app.get('/', function (req, res) {
+      res.send('Hello World!')
+  })
 
-module.exports = app;
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
