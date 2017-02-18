5fc39c56d7bfa4e12de5fd88c2d1ac3b62de98c8
Eric Dobyns <ericdobyns@gmail.com>
2017-02-17 21:41:31 -0800

    node test

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
-FROM alpine:3.4
-
-RUN apk --update add nginx php5-fpm && \
-    mkdir -p /var/log/nginx && \
-    touch /var/log/nginx/access.log && \
-    mkdir -p /run/nginx
-
-ADD www /www
-ADD nginx.conf /etc/nginx/
-ADD php-fpm.conf /etc/php5/php-fpm.conf
-
+FROM node:latest
+ADD package.json package.json
+RUN npm install
+ADD . .
 EXPOSE 80
-CMD php-fpm -d variables_order="EGPCS" && (tail -F /var/log/nginx/access.log &) && exec nginx -g "daemon off;"
+CMD ["npm", "start"]
---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+              
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+process.env.PORT = 8080;
+
+// Initialize Server
+app.listen(process.env.PORT, function() {
+  console.log('Listening on port:' + process.env.PORT);
+});
+ 
+// Handle Errors
+app.use(function(req, res) {
+  res.status(404);
+  res.sendFile('404.html', { root: require('path').join(__dirname, 'public/views/misc') });
+  console.log('Error: /404 loaded');
+});
+
+module.exports = app;
