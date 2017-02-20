5ba218d0a68c157226d404e7c5a25f7c816173b4
Eric Dobyns <ericdobyns@gmail.com>
2017-02-20 11:06:06 -0800

    1

--- tests/shellTest.sh
+++ tests/shellTest.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-url=”http://localhost:80“
-content=”$(curl -sLI “$url” | grep HTTP/1.1 | tail -1 | awk {‘print $2′})”
-if [ ! -z $content ] && [ $content -eq 200 ]
-then
-echo “valid url”
-else
-echo “invalid url”
-fi
+ping localhost:80
