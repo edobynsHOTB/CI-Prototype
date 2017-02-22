f79ada5e31027461b7c160e8b129e861b36d1df3
Eric Dobyns <ericdobyns@gmail.com>
2017-02-22 11:44:30 -0800

    123

--- src/Nodejs/app.js
+++ src/Nodejs/app.js
@@ -2,10 +2,13 @@
 
 var express = require('express')
 var app = express()
+var mongoose = require("mongoose");
 
-  app.get('/', function (req, res) {
-      res.send('This would be where someone might say, "Fuck you World"')
-  })
+mongoose.connect('mongodb://mongo:27017');
+
+app.get('/', function (req, res) {
+    res.send('This would be where someone might say, "Fuck you World" - connected to mongo...')
+})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 80!')
