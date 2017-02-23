c9e060a4e3da7ecfe4edcb315b93cbc7806d0348
Eric Dobyns <ericdobyns@gmail.com>
2017-02-22 16:21:10 -0800

    123

--- src/Nodejs/app.js
+++ src/Nodejs/app.js
@@ -6,8 +6,26 @@ var mongoose = require("mongoose");
 
 mongoose.connect('mongodb://mongo:27017');
 
+var schema = mongoose.Schema({ name: 'string' });
+var Event1 = mongoose.model('Event1', schema);
+
+var event1= new Event1({ name: 'something' });
+event1.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
+
 app.get('/', function (req, res) {
-    res.send('This would be where someone might say, "Fuck you World" - connected to mongo...');
+
+    const connection = mongoose.connection;
+    Object.keys(connection.models).forEach((collection) => {
+        // You can get the string name.
+        console.info(collection);
+        // Or you can do something else with the model.
+        res.send(collection);
+    });
+
+    // res.send('This would be where someone might say, "Fuck you World" - connected to mongo...')
 })
 
 app.listen(3000, function () {
